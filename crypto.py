85d6ef893c121a6e1d7850e79c82812edc553326
manjaro <galih.prastowo@gmail.com>
2019-05-02 13:25:41 +0700

    add timeout

--- crypto.py
+++ crypto.py
@@ -6,7 +6,7 @@ import locale
 
 locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
 try:
-	data = requests.get('https://indodax.com/api/btc_idr/ticker').text
+	data = requests.get('https://indodax.com/api/btc_idr/ticker', timeout=30).text
 	json = json.loads(data)
 	local_price = json["ticker"]["last"]
 	rupiah = locale.currency(int(local_price), grouping=True)

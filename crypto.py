f3baf0649c9235bd604ae01f6635abbf8d9929de
manjaro <galih.prastowo@gmail.com>
2019-05-02 09:01:00 +0700

    update 2 may 2019

--- crypto.py
+++ crypto.py
@@ -5,8 +5,11 @@ import json
 import locale
 
 locale.setlocale(locale.LC_ALL, 'id_ID.UTF-8')
-data = requests.get('https://indodax.com/api/btc_idr/ticker').text
-json = json.loads(data)
-local_price = json["ticker"]["last"]
-rupiah = locale.currency(int(local_price), grouping=True)
-print('BTC ' + rupiah.strip("0").replace(',',''))
+try:
+	data = requests.get('https://indodax.com/api/btc_idr/ticker').text
+	json = json.loads(data)
+	local_price = json["ticker"]["last"]
+	rupiah = locale.currency(int(local_price), grouping=True)
+	print('BTC ' + rupiah.strip("0").replace(',',''))
+except:
+	print('BTC Error')
